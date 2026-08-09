511a63fea3545d78e8099fd9b25c326955ca30ef
Chavez Harris <74829200+codedbychavez@users.noreply.github.com>
2026-08-09 14:48:40 -0400

    add Models folder and switched indentation to spacing in .editorconfig

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.MapOpenApi();
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
