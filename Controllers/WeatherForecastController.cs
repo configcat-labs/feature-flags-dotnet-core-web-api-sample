091cb2ed1c40cb44cc16dfa5888c289992677a5e
Chavez Harris <74829200+codedbychavez@users.noreply.github.com>
2026-08-09 05:24:42 -0400

    fixes

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,5 +1,8 @@
-using Microsoft.AspNetCore.Mvc;
 using ConfigCat.Client; // Import types from the ConfigCat SDK's main namespace
+
+using feature_flags_dotnet_core_web_api_sample.Models;
+
+using Microsoft.AspNetCore.Mvc;
 
 namespace feature_flags_dotnet_core_web_api_sample.Controllers;
 
@@ -9,39 +12,39 @@ namespace feature_flags_dotnet_core_web_api_sample.Controllers;
 // so we can use it within the class to control the API response
 public class WeatherForecastController(IConfigCatClient configCatClient) : ControllerBase
 {
-    private static readonly string[] Summaries =
-    [
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    ];
+	private static readonly string[] Summaries =
+	[
+		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+	];
 
-[HttpGet(Name = "GetWeatherForecast")]
-public async Task<IEnumerable<WeatherForecast>> Get()
-{
-    // A unique user id is required when creating a ConfigCat User Object
-    var configCatUser = new User("user-id-123")
-    {
-        Email = "john@example.com",
-        Country = "United Kingdom",
-        Custom =
-        {
-            // The dictionary keys you use here should match the custom comparison attributes
-            // you added to your flag's targeting rule on the ConfigCat Dashboard
-            ["accountType"] = "premium",
-        }
-    };
+	[HttpGet(Name = "GetWeatherForecast")]
+	public async Task<IEnumerable<WeatherForecast>> Get()
+	{
+		// A unique user id is required when creating a ConfigCat User Object
+		var configCatUser = new User("user-id-123")
+		{
+			Email = "john@example.com",
+			Country = "United Kingdom",
+			Custom =
+			{
+				// The dictionary keys you use here should match the custom comparison attributes
+				// you added to your flag's targeting rule on the ConfigCat Dashboard
+				["accountType"] = "premium",
+			}
+		};
 
-    // Get the flag's latest value
-    var isMyFeatureFlagEnabled = await configCatClient.GetValueAsync("myFeatureFlag", false, configCatUser);
+		// Get the flag's latest value
+		var isMyFeatureFlagEnabled = await configCatClient.GetValueAsync("myFeatureFlag", false, configCatUser);
 
-    // When the flag is off, return only a limited number of items
-    var numDays = isMyFeatureFlagEnabled ? 5 : 2;
+		// When the flag is off, return only a limited number of items
+		var numDays = isMyFeatureFlagEnabled ? 5 : 2;
 
-    return Enumerable.Range(1, numDays).Select(index => new WeatherForecast
-    {
-        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        TemperatureC = Random.Shared.Next(-20, 55),
-        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-    })
-    .ToArray();
-}
+		return Enumerable.Range(1, numDays).Select(index => new WeatherForecast
+		{
+			Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+			TemperatureC = Random.Shared.Next(-20, 55),
+			Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+		})
+		.ToArray();
+	}
 }
